f0cc2fde025f61d88e97547b1ee2b03c320fbf87
entin <entin@SABIN>
2016-11-01 18:40:03 -0700

    updated login component

--- App/Components/Login/Component.cs
+++ App/Components/Login/Component.cs
@@ -3,6 +3,8 @@ namespace Websilk.Components
 {
     public class Login: Component
     {
+        public string label = "Log In";
+
         public Login() { }
 
         public override string Path
@@ -15,10 +17,12 @@ namespace Websilk.Components
 
         public override void Load()
         {
-            var txtEmail = (Element.Textbox)Page.Elements.Load(ElementType.Textbox, "");
-            var txtPass = (Element.Textbox)Page.Elements.Load(ElementType.Textbox, "password");
-            scaffold.Data["field-email"] = txtEmail.Render("email", "", "email address", "");
-            scaffold.Data["field-pass"] = txtPass.Render("password", "","pa$$word","", Element.Textbox.enumTextType.password);
+            var txtEmail = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
+            var txtPass = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
+            var btnLogin = (Element.Button)Page.Elements.Load(ElementType.Button, "button-apply");
+            scaffold.Data["field-email"] = txtEmail.Render("email", "email", "", "email address");
+            scaffold.Data["field-pass"] = txtPass.Render("password", "password", "", "password", "", Element.Textbox.enumTextType.password);
+            scaffold.Data["button-login"] = btnLogin.Render("btnlogin", "javascript:S.login.submit()", label);
         }
     }
 }

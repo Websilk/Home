a6930ccd2566674d3a149c207475fa06963f0c19
Mark Entingh <entingh@gmail.com>
2016-11-02 08:49:03 -0700

    working on new password form (for admin only)

--- App/Components/Login/Component.cs
+++ App/Components/Login/Component.cs
@@ -17,12 +17,28 @@ namespace Websilk.Components
 
         public override void Load()
         {
-            var txtEmail = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
-            var txtPass = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
-            var btnLogin = (Element.Button)Page.Elements.Load(ElementType.Button, "button-apply");
-            scaffold.Data["field-email"] = txtEmail.Render("email", "email", "", "email address");
-            scaffold.Data["field-pass"] = txtPass.Render("password", "password", "", "password", "", Element.Textbox.enumTextType.password);
-            scaffold.Data["button-login"] = btnLogin.Render("btnlogin", "javascript:S.login.submit()", label);
+            if(S.Server.encryption.reset == true)
+            {
+                //load new password form (for admin only)
+                scaffold = new Scaffold(S, "/App/Components/Login/new-pass.html");
+                var txtPass = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
+                var txtPass2 = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
+                var btnLogin = (Element.Button)Page.Elements.Load(ElementType.Button, "button-apply");
+                scaffold.Data["field-pass"] = txtPass.Render("password", "password", "", "password", "", Element.Textbox.enumTextType.password);
+                scaffold.Data["field-pass2"] = txtPass2.Render("password2", "password", "", "retype password", "", Element.Textbox.enumTextType.password);
+                scaffold.Data["button-save"] = btnLogin.Render("btnsave", "javascript:S.login.savepass()", "Save");
+            }
+            else
+            {
+                //load login form (default)
+                var txtEmail = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
+                var txtPass = (Element.Textbox)Page.Elements.Load(ElementType.Textbox);
+                var btnLogin = (Element.Button)Page.Elements.Load(ElementType.Button, "button-apply");
+                scaffold.Data["field-email"] = txtEmail.Render("email", "email", "", "email address");
+                scaffold.Data["field-pass"] = txtPass.Render("password", "password", "", "password", "", Element.Textbox.enumTextType.password);
+                scaffold.Data["button-login"] = btnLogin.Render("btnlogin", "javascript:S.login.submit()", label);
+            }
+            
         }
     }
 }
